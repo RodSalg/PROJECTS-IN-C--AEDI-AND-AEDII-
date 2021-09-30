03edfbd321a465139e9671d19f679477b97c3ad8
Thiago Rodrigo Monteiro Salgado <thiago.salgado@icomp.ufam.edu.br>
2021-09-30 12:27:44 -0400

    terminando a lista 8 questao 1 finalizada

--- Dynamic structures, algorithms, and codes/Trees/GenericTree.c
+++ Dynamic structures, algorithms, and codes/Trees/GenericTree.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct arvgen{
+struct arvgen
+{
 
     char info;
     struct arvgen *prim;
@@ -10,46 +11,82 @@ struct arvgen{
 
 typedef struct arvgen ArvGen;
 
+ArvGen *cria(char c)
+{
 
-ArvGen* cria (char c){
-
-    ArvGen* a =(ArvGen*)malloc(sizeof(ArvGen));
+    ArvGen *a = (ArvGen *)malloc(sizeof(ArvGen));
 
     a->info = c;
     a->prim = NULL;
     a->prox = NULL;
 
     return a;
-
 }
 
-
-void insere(ArvGen* a, ArvGen* f){
+void insereInicio(ArvGen *a, ArvGen *f)
+{
 
     f->prox = a->prim;
     a->prim = f;
+}
+
+ArvGen *insere(ArvGen *pai, ArvGen *filho) //insere no final
+{
+
+    //para inserir no final
+
+    ArvGen *aux;
+   
+    if (pai->prim == NULL){ //verificar se é vazio
+
+        pai->prim = filho;
+        filho->prox = NULL;
+        filho->prim = NULL;
+
+        return filho;
+    }else{ //caso contrario significa que a lista possui pelo menos um elemento
+        aux = pai->prim;
+        
+
+        while (aux){
+            if (aux->prox == NULL){
+               
+                aux->prox = filho;
+                filho->prim = NULL;
+                filho->prox = NULL;
+                return filho;
+            }
+            else if (aux->prox != NULL){
+                aux = aux->prox;
+            }
+        }
+
+        return NULL;
+    }
 
 }
 
+void imprime(ArvGen *a)
+{
 
-void imprime(ArvGen* a){
-
-    ArvGen* p;
+    ArvGen *p;
     printf("%c\n", a->info);
 
-    for (p = a->prim; p != NULL; p = p->prox){
+    for (p = a->prim; p != NULL; p = p->prox)
+    {
         imprime(p);
     }
-    
 }
 
-void libera(ArvGen* a){
+void libera(ArvGen *a)
+{
 
-    ArvGen* p = a->prim;
+    ArvGen *p = a->prim;
 
-    while (p != NULL){
+    while (p != NULL)
+    {
 
-        ArvGen* t = p->prox;
+        ArvGen *t = p->prox;
         libera(p);
         p = t;
     }
@@ -57,40 +94,86 @@ void libera(ArvGen* a){
     free(a);
 }
 
+ArvGen *busca(ArvGen *pai, char code)
+{ //funcao de busca
 
-int main(){
+    if (pai != NULL)
+    { //verifica se o pai é nulo
+        if (pai->info == code)
+        {
+            return pai;
+        }
+        else
+        {
+            ArvGen *p;   // ponteiro usado para percorrer a lista sem perder a lista original com o ponteiro original
+            ArvGen *aux; //variavel que vai guardar o retorno caso encontre o valor
 
+            for (p = pai->prim; p != NULL; p = p->prox)
+            {                         //forma de percorrer uma arvore generica através de recursão
+                aux = busca(p, code); //guardando no auxiliar;
+                if (aux != NULL)
+                {
+                    return aux; //caso exista ele vai retornar o aux como resposta;
+                }
+            }
+        }
+        return busca(pai->prox, code);
+    }
 
+    return NULL;
+}
 
+int main()
+{
 
-    ArvGen* a = cria('a');
-    ArvGen* b = cria('b');
-    ArvGen* c = cria('c');
-    ArvGen* d = cria('d');
-    ArvGen* e = cria('e');
-    ArvGen* f = cria('f');
-    ArvGen* g = cria('g');
-    ArvGen* h = cria('h');
-    ArvGen* i = cria('i');
-    ArvGen* j = cria('j');
+    //agora eu preciso automatizar
 
+    char raizChar;    //raiz principal
+    char raizAuxChar; // raiz criada como folha da principal
+    char novoChar;
 
-    insere(a,b);
-    insere(b,c);
-    insere(a,f);
-    insere(a,g);
-    insere(b,e);
-    insere(c,d);
-    insere(g,h);
-    insere(g,i);
-    insere(i,j);
-    
-    
-    
-    
-    
+    ArvGen *raiz;
+    scanf(" %c%*[^\n]", &raizChar);
+    raiz = cria(raizChar); //raiz principal
 
-    imprime(a);
+    ArvGen *raizAux;
+    ArvGen *novo;
+    ArvGen *pai;
 
+    int flag = 0;
 
+    scanf(" %c", &novoChar);    //filho -> b
+    scanf(" %c", &raizAuxChar); //pai -> a
+
+    if (novoChar == '#' && raizAuxChar == '#')
+    {
+        flag = 1;
+    }
+
+    while (flag == 0)
+    {
+
+        novo = cria(novoChar);
+
+        ArvGen *aux;
+        aux = raiz;
+        pai = busca(aux, raizAuxChar);
+
+        if (pai == NULL)
+        {
+            printf("\n o pai sempre deve existir\n");
+            break;
+        }
+
+        insere(pai, novo);
+
+        scanf(" %c", &novoChar);    //filho -> b
+        scanf(" %c", &raizAuxChar); //pai -> a
+        if (novoChar == '#' && raizAuxChar == '#')
+        {
+            flag = 1;
+        }
+    }
+
+    imprime(raiz);
 }
--- aed2 codebench/lista8/questao1.c
+++ aed2 codebench/lista8/questao1.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct arvgen{
+struct arvgen
+{
 
     char info;
     struct arvgen *prim;
@@ -10,46 +11,90 @@ struct arvgen{
 
 typedef struct arvgen ArvGen;
 
+ArvGen *cria(char c)
+{
 
-ArvGen* cria (char c){
-
-    ArvGen* a =(ArvGen*)malloc(sizeof(ArvGen));
+    ArvGen *a = (ArvGen *)malloc(sizeof(ArvGen));
 
     a->info = c;
     a->prim = NULL;
     a->prox = NULL;
 
     return a;
-
 }
 
+ArvGen *insere(ArvGen *pai, ArvGen *filho)
+{ //estou sempre inserindo no inicio, preciso de inserir no final
+    //pai = c filho = D
+    /*printf("quanto vale o meu pai = %c\n\n", pai->info );
+    printf("quanto vale o meu filho = %c\n\n", filho->info );
+    printf("\n\n >>>>>> entrei na funcao insere <<<<<< \n");*/
+    //para inserir no inicio
+    /*f->prox = a->prim;
+    a->prim = f; */
 
-void insere(ArvGen* a, ArvGen* f){
+    //para inserir no final
 
-    f->prox = a->prim;
-    a->prim = f;
+    ArvGen *aux;
+    //printf("fiz ate aqui\n");
+    if (pai->prim == NULL)
+    { //verificar se é vazio
+        //printf("\to primeiro do pai eh nulo vou alocar no lugar del e\n");
+        pai->prim = filho;
+        filho->prox = NULL;
+        filho->prim = NULL;
+        return filho;
+    }
+    else
+    { //caso contrario significa que a lista possui pelo menos um elemento
+        aux = pai->prim;
+        //printf("\t existe pelo menos um elemento\n");
 
+        while (aux)
+        {
+            if (aux->prox == NULL)
+            {
+                //printf("\t o proximo vai ser nulo, o proximo vai se tornar o filho\n");
+                aux->prox = filho;
+                //printf("\t esse e o meu aux.prox = %c\n", aux->prox->info);
+                filho->prim = NULL;
+                filho->prox = NULL;
+                return filho;
+            }
+            else if (aux->prox != NULL)
+            {
+                //printf("\t vou saltar para procurar o proximo que sera nulo\n");
+                aux = aux->prox;
+            }
+        }
+
+        return NULL;
+    }
+
+    //printf("\n >>>>>> saida da funcao insere <<<<<< \n");
 }
 
+void imprime(ArvGen *a)
+{
 
-void imprime(ArvGen* a){
-
-    ArvGen* p;
+    ArvGen *p;
     printf("%c\n", a->info);
 
-    for (p = a->prim; p != NULL; p = p->prox){
+    for (p = a->prim; p != NULL; p = p->prox)
+    {
         imprime(p);
     }
-    
 }
 
-void libera(ArvGen* a){
+void libera(ArvGen *a)
+{
 
-    ArvGen* p = a->prim;
+    ArvGen *p = a->prim;
 
-    while (p != NULL){
+    while (p != NULL)
+    {
 
-        ArvGen* t = p->prox;
+        ArvGen *t = p->prox;
         libera(p);
         p = t;
     }
@@ -57,42 +102,94 @@ void libera(ArvGen* a){
     free(a);
 }
 
+ArvGen *achaPai(ArvGen *pai, char code)
+{ //funcao de busca
 
-int main(){
+    if (pai != NULL)
+    { //verifica se o pai é nulo
+        if (pai->info == code)
+        {
+            return pai;
+        }
+        else
+        {
+            ArvGen *p;   // ponteiro usado para percorrer a lista sem perder a lista original com o ponteiro original
+            ArvGen *aux; //variavel que vai guardar o retorno caso encontre o valor
 
+            for (p = pai->prim; p != NULL; p = p->prox)
+            {                           //forma de percorrer uma arvore generica através de recursão
+                aux = achaPai(p, code); //guardando no auxiliar;
+                if (aux != NULL)
+                {
+                    return aux; //caso exista ele vai retornar o aux como resposta;
+                }
+            }
+        }
+        return achaPai(pai->prox, code);
+    }
 
-    char pai;
-    char filho;
+    return NULL;
+}
 
-    scanf(" %c%*[^\n]",&pai);
-    ArvGen* a = cria(pai);
-    ArvGen*b;
+int main()
+{
 
-    
+    //agora eu preciso automatizar
+
+    char raizChar;    //raiz principal
+    char raizAuxChar; // raiz criada como folha da principal
+    char novoChar;
+
+    ArvGen *raiz;
+    scanf(" %c%*[^\n]", &raizChar);
+    raiz = cria(raizChar); //raiz principal
+
+    ArvGen *raizAux;
+    ArvGen *novo;
+    ArvGen *pai;
+
     int flag = 0;
-    if(pai == '#' && filho == '#'){
-        printf("entrei auqi");
+
+    scanf(" %c", &novoChar);    //filho -> b
+    scanf(" %c", &raizAuxChar); //pai -> a
+
+    if (novoChar == '#' && raizAuxChar == '#')
+    {
         flag = 1;
     }
 
-    while(flag == 0){
+    while (flag == 0)
+    {
 
-        scanf(" %c%*[^\n]",&pai);
-        scanf(" %c%*[^\n]",&filho);
- 
-        a = cria(pai);
-        printf("eu passo");
-        insere(a, filho);
+        novo = cria(novoChar);
 
-        if(pai == '#' && filho == '#'){
-            printf("\nachei\n");
-            flag = 1;
+        ArvGen *aux;
+        aux = raiz;
+        pai = achaPai(aux, raizAuxChar);
+
+        if (pai == NULL)
+        {
+            printf("\n o pai eh nulo\n");
+            break;
         }
 
+        /*printf("\n\n ***** debugando: ******\n");
+        printf("esse e meu pai: %c", pai->info);
+        printf("esse e meu filho: %c", novo->info);*/
+
+        insere(pai, novo);
+        /*printf(">>>>> minha arvore: \n");
+        imprime(raiz);
+        printf("fim da arvore <<<<<<<\n");*/
+
+        //condicao de parada
+        scanf(" %c", &novoChar);    //filho -> b
+        scanf(" %c", &raizAuxChar); //pai -> a
+        if (novoChar == '#' && raizAuxChar == '#')
+        {
+            flag = 1;
+        }
     }
 
-
-    imprime(a);
-
-
+    imprime(raiz);
 }
